72aac27c5a172c1387369080b168d8e87b728d64
Antonio Curavalea <one.kyonblack@gmail.com>
2026-02-11 19:53:51 +0200

    feat: only strip the first 3 frames

--- packages/echelon-server/src/commands.rs
+++ packages/echelon-server/src/commands.rs
@@ -95,7 +95,7 @@ pub async fn record_display(output_file: &str, frame_pipe_path: &str) -> anyhow:
             "-i",
             frame_pipe_path,
             "-vf",
-            "select='gte(n,10)',setpts=N/FRAME_RATE/TB",
+            "select='gte(n,2)',setpts=N/FRAME_RATE/TB",
             "-c:v",
             "libx264",
             "-preset",
